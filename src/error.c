c39599b61ef3d85ed31d4715ac8aff0e2cfb42eb
Ben Webb <benjamin.webb@global.ntt>
2020-05-04 14:58:00 +0200

    error messages for invalid input

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: ben <ben@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 11:19:07 by bwebb             #+#    #+#             */
-/*   Updated: 2020/05/04 14:45:38 by ben              ###   ########.fr       */
+/*   Updated: 2020/05/04 14:57:14 by ben              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,24 @@
 void	erexit(t_heart *heart, int errorcode)
 {
 	char	*errormessages[2];
-	
+
 	errormessages[0] = "Error: No Paths Found";
 	errormessages[1] = "Error: Malloc failed";
-	ft_putendl(errormessages[errorcode - 1]);
+	if (errorcode == 3)
+	{
+		if (!heart->inputchecks->ants)
+			ft_putendl("Error: ants missing or invalid");
+		if (!heart->inputchecks->rooms)
+			ft_putendl("Error: rooms missing or invalid");
+		if (!heart->inputchecks->links)
+			ft_putendl("Error: links missing or invalid");
+		if (!heart->inputchecks->start)
+			ft_putendl("Error: start missing or invalid");
+		if (!heart->inputchecks->end)
+			ft_putendl("Error: end missing or invalid");
+	}
+	else
+		ft_putendl(errormessages[errorcode - 1]);
 	freeheart(heart);
 	exit(1);
 }
--- src/lemon.c
+++ src/lemon.c
@@ -6,7 +6,7 @@
 /*   By: ben <ben@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 16:26:04 by bwebb             #+#    #+#             */
-/*   Updated: 2020/05/04 13:46:48 by ben              ###   ########.fr       */
+/*   Updated: 2020/05/04 14:51:48 by ben              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,15 +49,14 @@ int	main(void)
 	while (get_next_line(0, &(heart->buff)))
 		if (!addinputnode(&heart->input, heart->buff))
 			erexit(heart, 2);
-	if (validateinput(heart))
-	{
-		initroomnodes(heart);
-		heart->network = *heart->inputchecks->start;
-		putinputlist(heart->input, 0);
-		if(!bfs(heart))
-			erexit(heart, 1);
-		initants(heart);
-		qants(heart);
-	}
+	if (!validateinput(heart))
+		erexit(heart, 3);
+	initroomnodes(heart);
+	heart->network = *heart->inputchecks->start;
+	putinputlist(heart->input, 0);
+	if(!bfs(heart))
+		erexit(heart, 1);
+	initants(heart);
+	qants(heart);
 	freeheart(heart);
 }
