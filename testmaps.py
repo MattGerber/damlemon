a8e6fa280f34326f9ca12f3a3b74a27090c94abd
Ben Webb <benjamin.webb@global.ntt>
2020-06-01 10:41:29 +0200

    test maps in alphabetical order

--- testmaps.py
+++ testmaps.py
@@ -3,7 +3,7 @@ import subprocess
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 output = open(THIS_FOLDER + "/output.txt", "w+")
 my_lemin = os.path.join(THIS_FOLDER, "lem-in")
-for filename in os.listdir('/home/ben/Docs/personal/lemin/maps'):
+for filename in sorted(os.listdir('/home/ben/Docs/personal/lemin/maps')):
     my_file = os.path.join(THIS_FOLDER, "maps/" + filename)
     cmd = my_lemin + ' < ' + my_file
     so = os.popen(cmd).read()
